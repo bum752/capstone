fd70772f450dfd83ac9347f79178c8790fbd17c5
bum752 <bum752@gmail.com>
2018-10-29 11:51:02 +0900

    sqlit3 초기화 수정

--- src/sqlite3.js
+++ src/sqlite3.js
@@ -7,6 +7,11 @@ db.serialize(function() {
   db.get('SELECT name FROM sqlite_master WHERE type="table" AND name="alarm"', function(err, row) {
     if (row === undefined) {
       db.run('CREATE TABLE alarm (id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR(20), hour VARCHAR(2) NOT NULL, minute VARCHAR(2) NOT NULL)');
+    }
+  });
+
+  db.get('SELECT name FROM sqlite_master WHERE type="table" AND name="schedule"', function(err, row) {
+    if (row === undefined) {
       db.run('CREATE TABLE schedule (id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR(20), time DATETIME NOT NULL)');
     }
   });
